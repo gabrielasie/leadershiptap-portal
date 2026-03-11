5f1c7f994e8cb4e40d623b4652a7bc993b5437e7
gabrielasie <83417717+gabrielasie@users.noreply.github.com>
2026-03-11 17:01:43 +0100

    fix: use nodejs runtime for clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+export const runtime = 'nodejs'
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
